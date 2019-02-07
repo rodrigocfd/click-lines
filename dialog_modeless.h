38a62f486bd1c4a6ba60f3f5074fbba35d5d7b9e
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2019-02-07 17:46:39 -0200

    Squashed 'winlamb/' content from commit 77f4389
    git-subtree-dir: winlamb
    git-subtree-split: 77f4389eabf8ce644c195d9e32ab32beff9ecff5

---
+++ dialog_modeless.h
@@ -0,0 +1,84 @@
+/**
+ * Part of WinLamb - Win32 API Lambda Library
+ * https://github.com/rodrigocfd/winlamb
+ * Copyright 2017-present Rodrigo Cesar de Freitas Dias
+ * This library is released under the MIT License
+ */
+
+#pragma once
+#include "internals/base_dialog.h"
+#include "internals/base_loop.h"
+#include "internals/base_msg_impl.h"
+#include "internals/base_text_impl.h"
+#include "internals/base_thread_impl.h"
+#include "internals/styler.h"
+#include "wnd.h"
+
+namespace wl {
+
+// Inherit from this class to have a dialog modeless popup.
+class dialog_modeless :
+	public wnd,
+	public wli::base_msg_impl<INT_PTR>,
+	public wli::base_thread_impl<INT_PTR, TRUE>,
+	public wli::base_text_impl<dialog_modeless>
+{
+private:
+	HWND                            _hWnd = nullptr;
+	wli::base_msg<INT_PTR>          _baseMsg{_hWnd};
+	wli::base_thread<INT_PTR, TRUE> _baseThread{_baseMsg};
+	wli::base_dialog                _baseDialog{_hWnd, _baseMsg};
+	wli::base_loop*                 _pParentBaseLoop = nullptr;
+
+public:
+	// Defines window creation parameters.
+	wli::base_dialog::setup_vars setup;
+
+	// Wraps window style changes done by Get/SetWindowLongPtr.
+	wli::styler<dialog_modeless> style{this};
+
+protected:
+	dialog_modeless() :
+		wnd(_hWnd), base_msg_impl(_baseMsg), base_thread_impl(_baseThread), base_text_impl(_hWnd)
+	{
+		this->base_msg_impl::on_message(WM_CLOSE, [this](params) noexcept->INT_PTR {
+			DestroyWindow(this->_hWnd);
+			return TRUE;
+		});
+		this->base_msg_impl::on_message(WM_NCDESTROY, [this](params)->INT_PTR {
+			if (this->_pParentBaseLoop) {
+				this->_pParentBaseLoop->remove_modeless(this->_hWnd);
+			}
+			return TRUE;
+		});
+	}
+
+public:
+	dialog_modeless(dialog_modeless&&) = default;
+	dialog_modeless& operator=(dialog_modeless&&) = default; // movable only
+
+	// Creates the modeless dialog, returning immediately.
+	template<typename parentT>
+	void create(parentT* parent) {
+		if (!this->_baseDialog.create_dialog_param(this->setup, parent->hwnd())) {
+			throw std::system_error(GetLastError(), std::system_category(),
+				"CreateDialogParam failed for modeless dialog");
+		}
+
+		this->_pParentBaseLoop = parent->_baseLoop; // parent must have us as friend
+		this->_pParentBaseLoop->add_modeless(this->_hWnd);
+		ShowWindow(this->_hWnd, SW_SHOW);
+	}
+
+	// Creates the modeless dialog, returning immediately.
+	template<typename parentT>
+	void create(parentT* parent, POINT clientPos) {
+		this->create(parent);
+		POINT parentPos = clientPos;
+		ClientToScreen(parent->hwnd(), &parentPos); // now relative to parent
+		SetWindowPos(this->_hWnd, nullptr,
+			parentPos.x, parentPos.y, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
+	}
+};
+
+}//namespace wl
---
+++ subclass.h
@@ -0,0 +1,106 @@
+/**
+ * Part of WinLamb - Win32 API Lambda Library
+ * https://github.com/rodrigocfd/winlamb
+ * Copyright 2017-present Rodrigo Cesar de Freitas Dias
+ * This library is released under the MIT License
+ */
+
+#pragma once
+#include "internals/base_msg_impl.h"
+#include "internals/base_thread_impl.h"
+#include "wnd.h"
+#include <CommCtrl.h>
+
+namespace wl {
+
+// Manages window subclassing for a window.
+class subclass final :
+	public wnd,
+	public wli::base_msg_impl<LRESULT>,
+	public wli::base_thread_impl<LRESULT, 0>
+{
+private:
+	HWND                         _hWnd = nullptr;
+	UINT                         _subclassId = -1;
+	wli::base_msg<LRESULT>       _baseMsg{_hWnd};
+	wli::base_thread<LRESULT, 0> _baseThread{_baseMsg};
+
+public:
+	~subclass() {
+		this->remove_subclass();
+	}
+
+	subclass() :
+		wnd(_hWnd), base_msg_impl(_baseMsg), base_thread_impl(_baseThread) { }
+
+	subclass(subclass&&) = default;
+	subclass& operator=(subclass&&) = default; // movable only
+
+	void remove_subclass() noexcept {
+		if (this->hwnd()) {
+			RemoveWindowSubclass(this->hwnd(), _subclass_proc, this->_subclassId);
+			this->_hWnd = nullptr; // clear HWND
+		}
+	}
+
+	void install_subclass(HWND hCtrl) {
+		if (this->hwnd()) {
+			throw std::logic_error("Trying to install subclass twice.");
+		} else if (!hCtrl) {
+			throw std::invalid_argument("Trying to install subclass on an empty control.");
+		}
+
+		this->_hWnd = hCtrl; // store HWND
+		if (hCtrl) {
+			this->_subclassId = _next_id();
+			SetWindowSubclass(hCtrl, _subclass_proc, this->_subclassId,
+				reinterpret_cast<DWORD_PTR>(this));
+		}
+	}
+
+	void install_subclass(HWND hParent, int ctrlId) {
+		this->install_subclass(GetDlgItem(hParent, ctrlId));
+	}
+
+	void install_subclass(const wnd& ctrl) {
+		this->install_subclass(ctrl.hwnd());
+	}
+
+	void install_subclass(const wnd& parent, int ctrlId) {
+		this->install_subclass(GetDlgItem(parent.hwnd(), ctrlId));
+	}
+
+private:
+	static LRESULT CALLBACK _subclass_proc(HWND hWnd, UINT msg,
+		WPARAM wp, LPARAM lp, UINT_PTR idSubclass, DWORD_PTR refData) noexcept
+	{
+		subclass* pSelf = reinterpret_cast<subclass*>(refData);
+
+		if (pSelf) {
+			if (pSelf->hwnd()) {
+				std::pair<bool, LRESULT> procRet =
+					pSelf->_baseMsg.process_msg(msg, wp, lp); // catches all message exceptions internally
+
+				if (msg == WM_NCDESTROY) {
+					pSelf->remove_subclass();
+				}
+				if (procRet.first) {
+					return procRet.second; // message was processed
+				}
+			} else if (msg == WM_NCDESTROY) {
+				pSelf->remove_subclass();
+			}
+		} else if (msg == WM_NCDESTROY) {
+			RemoveWindowSubclass(hWnd, _subclass_proc, idSubclass);
+		}
+
+		return DefSubclassProc(hWnd, msg, wp, lp); // message was not processed
+	}
+
+	static UINT _next_id() noexcept {
+		static UINT firstId = 0;
+		return firstId++;
+	}
+};
+
+}//namespace wl
